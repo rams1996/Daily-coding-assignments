f32da38800c20ce132312bb2d23865252f31d1c1
Ram Sethuraman <ramsethuraman123@gmail.com>
2021-03-18 18:57:44 -0500

    Time: 2336 ms (30.34%), Space: 17.4 MB (87.38%) - LeetHub

--- 3sum/3sum.py
+++ 3sum/3sum.py
@@ -2,29 +2,29 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
         result=[]
-        duplicate=set()
-        for i in range(len(nums)-2):
-            j=i+1
-            k=len(nums)-1
-            while j<k:
-                if nums[i]+nums[j]+nums[k]<0:
-                    j+=1
-                elif nums[i]+nums[j]+nums[k]==0:
-                    temp=[]
-                    temp.extend([nums[i],nums[j],nums[k]])
-                    temp.sort()
-                    if tuple(temp) not in duplicate:
-                        result.append(temp)
-                        duplicate.add(tuple(temp))
-                    j+=1
-                    k-=1
-                    
-                else:
-                    k-=1
-        return result
-                    
+        i=0
+        while i<len(nums):
+            while i!=0 and i<len(nums) and nums[i]==nums[i-1]:
+                i+=1
+            left=i+1
+            right=len(nums)-1
             
+            while left<right:
                 
-                    
-                
+                while left<right and left!=i+1 and nums[left]==nums[left-1]:
+                    left+=1
+                while left<right and right!=len(nums)-1 and nums[right]==nums[right+1]:
+                    right-=1
+                if left<right:
+                    if nums[i]+nums[left]+nums[right]==0:
+                        result.append([nums[i],nums[left],nums[right]])
+                        left+=1
+                        right-=1
+                    elif nums[i]+nums[left]+nums[right]>0:
+                        right-=1
+                    else:
+                        left+=1
+            i+=1
+        return result
+            
         
